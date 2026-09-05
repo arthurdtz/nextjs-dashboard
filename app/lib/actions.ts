58b19f29a9691e58d04ac33db7d78e47c9931367
root <root@Arthur.localdomain>
2026-09-05 02:44:17 -0300

    feat: complete dashboard with authentication and invoice management

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
@@ -136,5 +139,25 @@ export async function deleteInvoice(id: string) {
         return {
             message: 'Database Error: Failed to Delete Invoice.',
         };
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+
+        throw error;
     }
 }
